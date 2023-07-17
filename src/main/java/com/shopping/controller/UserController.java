413adff698dbe79e541434b43d5d4f3b7bd239d3
“onble” <62949816+onble@users.noreply.github.com>
2023-07-17 08:14:54 +0800

    完善项目

--- src/main/java/com/shopping/controller/UserController.java
+++ src/main/java/com/shopping/controller/UserController.java
@@ -43,20 +43,23 @@ public class UserController {
     @RequestMapping("/wxlogin")
     //@RequestBody注解：接收前端通过请求传到后端的jsons字符串数据
     public R wxLogin(@RequestBody WxUserInfo wxUserInfo) {
+        System.out.println("i am here--------");
+        System.out.println(wxUserInfo);
 
         //1 利用前端传递过来的参数数据，拼接一个访问微信小程接口的访问地址，获取openId,用于登录下订单
         String jscode2sessionUrl = weixinProperties.getJscode2sessionUrl() + "?appid=" + weixinProperties.getAppid() + "&secret=" + weixinProperties.getSecret() + "&js_code=" + wxUserInfo.getCode() + "&grant_type=authorization_code";
-        System.out.println(jscode2sessionUrl);
+//        System.out.println(jscode2sessionUrl);
 
         //2 后端发送请求访问微信小程序获取openId的接口，返回一个openId
         String result = httpClientUtil.sendHttpGet(jscode2sessionUrl);
+        System.out.println("从wx获取的结果:");
         System.out.println(result);
 
         //3 调用JSON转换工具类，把json字符串转换为对象类型，在从中获取openId的值
         //jsonObject.get():该方法获取openId值后，返回值为object类型，所以调用toString()方法转为字符串
         JSONObject jsonObject = JSON.parseObject(result);
         String openid = jsonObject.get("openid").toString();
-        System.out.println(openid);
+//        System.out.println(openid);
 
         //4 判断如果数据库中没有对应openId的用户信息，则添加，如果有则更新用户信息（例如用户名或者登录最后事件等）
         // 1 插入用户到数据库 判断用户是否存在 不存在的话 插入 存在的话 更新
@@ -64,14 +67,14 @@ public class UserController {
                 QueryWrapper<WxUserInfo>().eq("openid", openid));
 
         if (resultWxUserInfo == null) { // 不存在 插入用户
-            System.out.println("不存在 插入用户");
+//            System.out.println("不存在 插入用户");
             wxUserInfo.setOpenid(openid);
             wxUserInfo.setRegisterDate(new Date());
             wxUserInfo.setLastLoginDate(new Date());
             //save()方法：把wxUserInfo对象插入到数据库中,是myBatisPlus中提供的插入数据的方法
             wxUserInfoService.save(wxUserInfo);
         } else { // 存在 更新用户信息
-            System.out.println("存在 更新用户信息");
+//            System.out.println("存在 更新用户信息");
             resultWxUserInfo.setNickName(wxUserInfo.getNickName());
             resultWxUserInfo.setAvatarUrl(wxUserInfo.getAvatarUrl());
             resultWxUserInfo.setLastLoginDate(new Date());
